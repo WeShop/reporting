af5f07b0f7c78faad03cb85845a453946da3be0e
Ilya Katz <ilya.katz@weshop.us>
2011-03-03 11:39:31 -0500

    switched to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reporting::Application.config.secret_token = '4e7190eec96f0a36735e38fbbf9a8cfe0ca84c77c561d52b08938090409bbc0a5ffdcc0d556d0b7c60d52db4864959c786ac1acabbf6d038213e82a09b7a948d'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_reporting_session',
-  :secret      => '9fec814397c172a03dbad127c8466dddf8d75b8216e6c7813e833be12dcc94080ef9e1014cc6345f7d330a423c5acb912efd33e5199686c2215e7ddabd0bce0d'
-}
+Reporting::Application.config.session_store :cookie_store, :key => '_reporting_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Reporting::Application.config.session_store :active_record_store
